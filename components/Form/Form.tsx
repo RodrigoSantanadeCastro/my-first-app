a20e1a38c1a256b2437fbbc316098e443a2bc0a5
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-09-26 22:05:30 -0300

    segunda aula

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -3,31 +3,50 @@ import { View,Text, Pressable } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 export function Form(){
-    const [side, setSide] = useState('');
-    const [area,setArea]  = useState('');
+    const [heigth,setHeigth] = useState('');
+    const [weigth,setWeigth] = useState('');
+    const [imc,setImc] = useState('')
+    
+    function imcCalculator()
+    {
+        let totalImc = (weigth/(heigth*heigth)).toFixed(2)
 
-    function calcularArea(){
-       let result = ((side ** 2) * Math.sqrt(3)) /4;
+        setImc(totalImc)
+    }
+    function validatorImc()
+    {
+        if(weigth != '' && heigth != ''){
+            imcCalculator()
+                setHeigth('')
+                setWeigth('')
+            
 
-       setArea(result);
-       
+        }
     }
 
     return(
         <View>
             <View>
-                <Text>Lado:</Text>
+                <Text>Altura:</Text>
                 <TextInput
-                onChangeText={setSide}
-                placeholder="Digite o lado em cm"
-                inputMode="numeric"
-                value={side}
+                    placeholder="EX. 1.75"
+                    inputMode="numeric"
+                    value={heigth}
 
                 />
-                <Pressable
-                    title="calcular"
-                    onPress={()=> calcularArea()}
+                <Text>Peso:</Text>
+                <TextInput
+                    placeholder="EX. 67.5"
+                    inputMode="numeric"
+                    value={weigth}
                 />
+               <Pressable>
+                    onPress={() => validatorImc}
+                    
+                    <Text>Calcular</Text>
+               </Pressable>
+
+               <Text>{imc}</Text>
             </View>
         </View>
     )
