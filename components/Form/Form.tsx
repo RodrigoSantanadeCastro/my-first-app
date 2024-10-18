1fa4bdda81dab1c25741ed8ece4047717a39d7d6
RodrigoSantanadeCastro <rodrigo.santana58@etec.sp.gov.br>
2024-10-17 22:12:30 -0300

    17-10

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,53 +1,131 @@
 import React, { useState } from "react";
-import { View,Text, Pressable } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
-export function Form(){
-    const [heigth,setHeigth] = useState('');
-    const [weigth,setWeigth] = useState('');
-    const [imc,setImc] = useState('')
+export function Form() {
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+    const [imc, setImc] = useState('');
+    const [imcList , setImcList] = useState([])
     
-    function imcCalculator()
-    {
-        let totalImc = (weigth/(heigth*heigth)).toFixed(2)
 
-        setImc(totalImc)
-    }
-    function validatorImc()
-    {
-        if(weigth != '' && heigth != ''){
-            imcCalculator()
-                setHeigth('')
-                setWeigth('')
-            
+function validatorImc()
+{
+    
+  if(height !== undefined && weight !== undefined){
+    let totalImc = (weight/(height*height)).toFixed(2)
 
-        }
-    }
+    imcList.push(totalImc)
+    setImc(totalImc) 
+    
+    setHeight('')
+    setWeight('')
+  }
+}
 
-    return(
-        <View>
-            <View>
-                <Text>Altura:</Text>
+
+ 
+    return (
+        <View style = {styles.form}>
+            <View style={styles.form}>
+                <Text style={styles.formLabel}>Altura:</Text>
                 <TextInput
-                    placeholder="EX. 1.75"
+                    onChangeText={setHeight}
                     inputMode="numeric"
-                    value={heigth}
-
+                    placeholder="Ex. 1.75"
+                    value={height} 
+                    style={styles.formInput}
                 />
+                
                 <Text>Peso:</Text>
                 <TextInput
-                    placeholder="EX. 67.5"
+                    onChangeText={setWeight}
                     inputMode="numeric"
-                    value={weigth}
+                    placeholder="Ex. 67.5"
+                    value={weight} 
+                    style={styles.formInput}
                 />
-               <Pressable>
-                    onPress={() => validatorImc}
-                    
-                    <Text>Calcular</Text>
-               </Pressable>
-
-               <Text>{imc}</Text>
+                <Pressable
+                onPress={() => validatorImc()}
+                  style={styles.formButton}
+                  >
+                    <Text style={styles.formButtonText}>Calcular</Text>
+                </Pressable>
+                <Text style = {styles.formIMC}>{imc}</Text>
+                <FlatList 
+                data={imcList.reverse()}
+                renderItem={({item}) => {
+                    return(
+                        <View>
+                            <Text>{item}</Text>
+                        </View>
+                    )
+                }}
+                />
             </View>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    formContext: {
+        width: "100%",
+        height: "100%",
+        bottom: 0,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        marginTop: 30
+    },
+    form: {
+       width: "100%",
+       height: "auto",
+       marginTop: 30,
+       padding: 10
+    },
+    
+    formLabel:{
+        color: "#00000",
+        fontSize: 18,
+        paddingLeft: 20
+    },
+    formInput:{
+        width: "90%",
+        height: 40,
+        borderRadius: 30,
+        backgroundColor: "#f6f6f6",
+        margin: 12,
+        paddingLeft: 10,
+    },
+    formButton:
+    {
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+    backgroundColor: "#ff0043",
+    paddingTop: 14,
+    paddingBottom: 14,
+    marginLeft: 12,
+    margin: 30
+    },
+
+    formButtonText:{
+     fontSize: 20   
+    },
+
+    formIMC:{
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems: "center",
+        width: "100%"
+
+
+    }    
+    }
+
+);
+
